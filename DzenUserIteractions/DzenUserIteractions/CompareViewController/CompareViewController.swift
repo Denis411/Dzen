efc443ab94af3145fda6c83e41aa395abff198c2
FoxyWind <ponomarevoleg89@gmail.com>
2023-01-09 16:41:39 +0500

    dev

--- DzenUserIteractions/DzenUserIteractions/CompareViewController/CompareViewController.swift
+++ DzenUserIteractions/DzenUserIteractions/CompareViewController/CompareViewController.swift
@@ -21,10 +21,20 @@ class CompareViewController: UIViewController {
 
 extension CompareViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        switch tableView {
+        case firstTableView: return 1
+        case secondTableView: return 1
+        default: return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        switch tableView {
+        case firstTableView: let cell = firstTableView.dequeueReusableCell(withIdentifier: "CompareTableViewCell", for: indexPath) as! CompareTableViewCell
+            return cell
+        case secondTableView: let cell = secondTableView.dequeueReusableCell(withIdentifier: "CompareTableViewCell", for: indexPath) as! CompareTableViewCell
+            return cell
+        default: return UITableViewCell()
+        }
     }
 }
